4f18e0b2dd98f0751082d9e7badae54b020e5a8d
Leo Lee <lidan@onething.com>
2019-05-29 16:13:20 +0800

    update: 代码格式化

---
+++ machoview/PreferenceController.mm
@@ -0,0 +1,23 @@
+/*
+ *  PreferencesWindowController.mm
+ *  MachOView
+ *
+ *  Created by psaghelyi on 12/24/12.
+ *
+ */
+
+#import "PreferenceController.h"
+
+@implementation MVPreferenceController
+
+- (id)init {
+    self = [super initWithWindowNibName:@"Preferences"];
+    return self;
+}
+
+- (IBAction)toggleOpenAtLaunch:(id)sender {
+    // nothing to do here?
+}
+
+@end
+
